8e511818dc48aa3ff552c5c79f6c7d0ba33aeaba
turnesar <turnesar@oregonstate.edu>
2020-01-18 16:18:56 -0800

    second commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render(){
+	return <div> TESTING 1 2 3! </div>;
+  } 	  
+}
+
+//add all the front end representation here
+export default App;
