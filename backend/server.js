e07fd58c351b1a9dfdf6c55c05edcabc3de04c18
Sarah Turner <turnersj2@gmail.com>
2020-02-18 20:26:08 -0800

    updated file

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,175 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const bodyParser = require('body-parser');
-const logger = require('morgan');
-const Data = require('./data');
+"use strict";
+var express = require('express');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+//set up our pool here 
+var pool = mysql.createPool({
+  //add in pool data or connect it with separate file 
+});
 
-const API_PORT = 3001; 
-const app = express();
-const router = express.Router();
 
-//MongoDb
 
-const databaseRoute =
-//add our MongoDB in here 
+var app = express();
 
-mongoose.connect(databaseRoute, { useNewUrlParser: true });
-
-let db = mongoose.connection;
-
-db.once('open', () => console.log('I am connected!'));
-
-db.on('error', console.error.bind(console, 'not connected :( -->'));
-
-app.use(bodyParser.urlencoded({ extended: false}));
+//uses second argument to set port
+app.set('port', process.argv[2]);
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(logger('dev'));
 
-//add in routes & methods here
+  /**
+   * ADD HEADERS
+   */
+app.use((req,res,next)=>  {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+/**
+   * STARTS UP SERVER ACCEPTING REQUESTS
+**/
+
+var server = app.listen(app.get('port'),() => {
+    var port = server.address().port;
+    console.log('Express started on port ' + port);
+});
 
 
-app.use("/api", router);
+//also need to add post for login and post for signup.
 
-app.listen(API_PORT, () => console.log('Im up on port ${API_PORT}'));
+  /**  SUBSCRIPTION QUERIES ****************************
+   * GET ALL SUBSCRIPTION DATA 
+   * SELECT QUERY
+   */
+app.get('/main',(req,res)=> {
+    var subs_sql = "put query here ";   
+    pool.query(subs_sql,(err,rows,result,fields)=>{
+        if(err)
+        {
+            res.json(err);
+            console.log(err);
+            return;
+        }
+        console.log(rows);
+        res.json(rows);
+    })
+});
+  /**  SUBSCRIPTION QUERIES ****************************
+   * GET  SORTED SUBSCRIPTION FROM SUBSCRIPTION TABLE WITH JOINS
+   * SELECT QUERY
+   */
+app.get('/main/vendor',(req,res)=> {
+    var subsV_sql = "put query here";   
+    pool.query(subsV_sql,[req.query.vendor_name],(err,rows,result,fields)=>{
+        if(err)
+        {
+            res.json(err);
+            console.log(err);
+            return;
+        }
+        console.log(rows);
+        res.json(rows);
+    })
+});
+
+ 
+ /**
+   *INSERT SUBSCRIPTION 
+   * 
+ */
+  app.post('/subscription',(req,res)=>{
+    let subData = [req.body.name, req.body.vendor,req.body.startDate, req.body.price, req.body.recurrence];
+    var insertSub ="insert query here"
+    pool.query(insertSub,[subData],(err,rows,result,fields)=>{
+        if(err)
+        {
+            res.json(err);
+            console.log(err);
+            return;
+        }
+        console.log(rows);
+        res.json(rows);
+    })
+});
+
+
+ /**
+   * UPDATE subscription
+   * 
+   */
+  app.put('/subscription',(req,res)=>{
+    var updatedSub = [req.body.name, req.body.vendor, req.body.startDate, req.body.price, req.body.recurrence, req.body.id];
+    var updatesql = "update query";
+    pool.query(updatesql,updatedSub,(err,rows,result,fields)=>{
+        if(err)
+        {
+            res.json(err);
+            console.log(err);
+            return;
+        }
+        console.log(rows);
+        res.json(rows);
+    })
+});
+
+   /**
+   * DELETE CHARACTER
+   *   
+   */
+  app.delete('/subscription/:id',(req,res)=>{
+    pool.query('DELETE FROM `Subscription` WHERE  id = ?',[req.params.id],(err,rows,result,fields)=>{
+        if(err)
+        {
+            res.send(err);
+            console.log(err);
+            return;
+        }
+        console.log('deleted successfully');
+        res.send('deleted successfully');
+   })
+});
+  
+   
+ 
+ /**
+   * GET total costs for all vendor for full time of subscription
+   * TO USE IN COST LIST 
+   */
+  app.get('/costs/:allTime',(req,res)=> {
+    pool.query("put query here",[req.params.user_id],(err,rows,result,fields)=>{
+        if(err)
+        {
+            res.json(err);
+            console.log(err);
+            return;
+        }
+        console.log(rows);
+        res.json(rows);
+    })
+});
+
+//404 Page;
+app.use(function(req, res){
+    res.status(404);
+    res.render('404');
+  });
+  
+  //500 Page
+  app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.render('500');
+  });
